8363383c3e9f195e6150152995e0dbb2e5f7bab4
Jiri Sedlacek <sej@sqldep.com>
2015-11-24 14:18:06 +0100

    Remove blacklists from oracle schemas

--- sql/oracle/queries/cmd.sql
+++ sql/oracle/queries/cmd.sql
@@ -2,139 +2,29 @@ select
   src.TEXT as sourceCode,
   src.NAME as name,
   src.TYPE as groupName,
-  '""DBNAME""' as databaseName,
+  '##DBNAME##' as databaseName,
   src.OWNER as schemaName
 from all_source src
-where instr(
-	-- blacklist of schemas to export
-	'ADAMS
-     ANONYMOUS
-     AURORA$ORB$UNAUTHENTICATED
-     AWR_STAGE
-     BLAKE
-     CLARK
-     CLOTH
-     CSMIG
-     CTXSYS
-     DBSNMP
-     DEMO
-     DIP
-     DMSYS
-     DSSYS
-     EXFSYS
-     HR
-     JONES
-     LBACSYS
-     MDSYS
-     OE
-     ORACLE_OCM
-     ORDPLUGINS
-     ORDSYS
-     OUTLN
-     PAPER
-     PERFSTAT
-     SCOTT
-     SH
-     STEEL
-     SYS
-     SYSTEM
-     TRACESVR
-     TSMSYS
-     WMSYS
-     WOOD
-     XDB'
-	,src.OWNER) = 0-- black list of oracle default schemas
- 	and src.TYPE in ('FUNCTION','PROCEDURE','PACKAGE BODY','PACKAGE')
+where
+  src.TYPE in ('FUNCTION','PROCEDURE','PACKAGE BODY','PACKAGE')
 order by src.OWNER,src.name,src.TYPE,src.line;
 
 --split
 
 select
-	TO_LOB(v.text) sourceCode,
+	v.text sourceCode,
 	v.VIEW_NAME as name,
 	v.OWNER||'.'||v.VIEW_NAME as groupName,
 	v.OWNER as schemaName,
 	'##DBNAME##' as databaseName
-  from all_views v
-  where instr('ADAMS
-     ANONYMOUS
-     AURORA$ORB$UNAUTHENTICATED
-     AWR_STAGE
-     BLAKE
-     CLARK
-     CLOTH
-     CSMIG
-     CTXSYS
-     DBSNMP
-     DEMO
-     DIP
-     DMSYS
-     DSSYS
-     EXFSYS
-     HR
-     JONES
-     LBACSYS
-     MDSYS
-     OE
-     ORACLE_OCM
-     ORDPLUGINS
-     ORDSYS
-     OUTLN
-     PAPER
-     PERFSTAT
-     SCOTT
-     SH
-     STEEL
-     SYS
-     SYSTEM
-     TRACESVR
-     TSMSYS
-     WMSYS
-     WOOD
-     XDB',v.OWNER) = 0;
+  from all_views v;
 
 --split
 
 select
-	TO_LOB(mv.QUERY) sourceCode,
+	mv.QUERY sourceCode,
 	mv.MVIEW_NAME as name,
 	mv.OWNER||'.'||mv.MVIEW_NAME as groupName,
 	mv.OWNER as schemaName,
 	'##DBNAME##' as databaseName
-  from all_mviews mv
-  where instr('ADAMS
-     ANONYMOUS
-     AURORA$ORB$UNAUTHENTICATED
-     AWR_STAGE
-     BLAKE
-     CLARK
-     CLOTH
-     CSMIG
-     CTXSYS
-     DBSNMP
-     DEMO
-     DIP
-     DMSYS
-     DSSYS
-     EXFSYS
-     HR
-     JONES
-     LBACSYS
-     MDSYS
-     OE
-     ORACLE_OCM
-     ORDPLUGINS
-     ORDSYS
-     OUTLN
-     PAPER
-     PERFSTAT
-     SCOTT
-     SH
-     STEEL
-     SYS
-     SYSTEM
-     TRACESVR
-     TSMSYS
-     WMSYS
-     WOOD
-     XDB',mv.OWNER) = 0;
+  from all_mviews mv;
