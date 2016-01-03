28dc3e52027949b2ce0f13839e0f6c657d7e8caf
danplaner <dplaner@post.cz>
2016-01-03 13:02:10 +0100

    CODE: draft cmd line parameters

--- SQLDepCmd/Program.cs
+++ SQLDepCmd/Program.cs
@@ -3,37 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Mono.Options;
+using SQLDepLib;
 
 namespace SQLDepCmd
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            bool show_help = false;
-            List<string> names = new List<string>();
-            int repeat = 1;
-            int verbosity = 0;
+            string dbType = string.Empty;
+            string hostName = string.Empty;
+            string auth_type = "sql_auth";
+            string server = string.Empty;
+            string port = string.Empty;
+            string database = string.Empty;
+            string loginName = string.Empty;
+            string loginpassword = string.Empty;
+            string customSqlSetName = string.Empty;
+            string exportFileName = string.Empty;
+            string sMyKey = string.Empty;
+            Guid myKey;
 
             var p = new OptionSet() {
-                { "n|name=", "the {NAME} of someone to greet.", v => names.Add (v) },
-                { "r|repeat=",  "the number of {TIMES} to repeat the greeting.\n" + "this must be an integer.", (int v) => repeat = v },
-                { "v", "increase debug message verbosity", v => { if (v != null) ++verbosity; } },
-                { "h|help",  "show this message and exit", v => show_help = v != null },
+                { "db|dbType=", "database type MsSQL(mssql)/Oracle(oracle)", v => dbType = v },
+                { "h|hostName=",  "hostname", v => hostName = v },
+                { "a|auth=",  "authorization SQL(default: sql_auth)/Windows (win_auth)", v => { if (v != null) auth_type = v; } },
+                { "s|server=",  "server", v => server = v },
+                { "p|port=",  "port", v => { if ( v != null) port = v; } },
+                { "d|database=",  "database", v => database = v },
+                { "u|user=",  "loginName", v => { if ( v != null) loginName = v; } },
+                { "pwd|password=",  "loginpassword", v => { if (v != null) loginpassword = v; } },
+                { "n|name=",  "name", v => customSqlSetName = v },
+                { "f|file=",  "file", v => exportFileName = v },
+                { "k|key=",  "key (Guid)", v => sMyKey = v },
             };
 
-            List<string> extra;
             try
             {
-                extra = p.Parse(args);
+                p.Parse(args);
+
+                myKey = Guid.Parse(sMyKey);
+
+                DBExecutor dbExecutor = new DBExecutor();
+                string connectString = dbExecutor.BuildConnectionString(dbType, auth_type, server, port, database, loginName, loginpassword);
+                dbExecutor.ConnectString = connectString;
+                dbExecutor.Hostname = hostName;
+                dbExecutor.Connect();
+
+                Executor executor = new Executor(dbExecutor);
+                executor.Run(customSqlSetName, myKey, dbType, exportFileName);
+
+
             }
-            catch (OptionException e)
+            catch (Exception e)
             {
-                Console.Write("greet: ");
                 Console.WriteLine(e.Message);
-                Console.WriteLine("Try `greet --help' for more information.");
-                return;
+                return -1;
             }
+
+            return 0;
         }
     }
 }
