b5e24af934d4f5306331d3ea67105f27e7cbdcf7
Jiri Sedlacek <sej@sqldep.com>
2016-01-19 15:51:19 +0100

    Restructure teradata selects
    - perform in this order
       1/ databases
       2/ databaseModel
       3/ queries - this step requires input from step 2 which is different
          from MS or Oracle selects which could be run without any input

--- sql/teradata/databaseModel/cmd.sql
+++ sql/teradata/databaseModel/cmd.sql
@@ -1,11 +1,5 @@
--- 1. seznam vsech tabulek, view a procedur - kvuli creatorname, coz je schema a kvuli rozeznani, co je Table, View a Procedure
-SELECT '##DBNAME##', creatorname, tablename from dbc.tables WHERE TABLEKIND = 'T';
+SELECT '##DBNAME##', tablekind, creatorname, tablename from dbc.tables WHERE TABLEKIND = 'T' OR TABLEKIND = 'V';
 
---split
-
-SELECT '##DBNAME##', creatorname, tablename from dbc.tables WHERE TABLEKIND = 'V';
-
--- 2. vytahnout datove typy sloupcu
 --split
 
 SELECT
@@ -20,8 +14,8 @@ TABLENAME,
 COLUMNNAME,
 CASE WHEN COLUMNTYPE='CF' THEN 'CHAR'
      WHEN COLUMNTYPE='CV' THEN 'VARCHAR'
-     WHEN COLUMNTYPE='D' THEN 'DECIMAL' 
-     WHEN COLUMNTYPE='TS' THEN 'TIMESTAMP'      
+     WHEN COLUMNTYPE='D' THEN 'DECIMAL'
+     WHEN COLUMNTYPE='TS' THEN 'TIMESTAMP'
      WHEN COLUMNTYPE='I' THEN 'INTEGER'
      WHEN COLUMNTYPE='I2' THEN 'SMALLINT'
      WHEN COLUMNTYPE='DA' THEN 'DATE'
@@ -29,20 +23,10 @@ END AS COLUMNTYPE,
 CASE WHEN COLUMNTYPE='CF' THEN COLUMNLENGTH
      WHEN COLUMNTYPE='CV' THEN COLUMNLENGTH
      WHEN COLUMNTYPE='D' THEN (DECIMALTOTALDIGITS||','||DECIMALFRACTIONALDIGITS)
-     WHEN COLUMNTYPE='TS' THEN COLUMNLENGTH      
+     WHEN COLUMNTYPE='TS' THEN COLUMNLENGTH
      WHEN COLUMNTYPE='I' THEN DECIMALTOTALDIGITS
      WHEN COLUMNTYPE='I2' THEN DECIMALTOTALDIGITS
      WHEN COLUMNTYPE='DA' THEN NULL
 END AS COLUMNNUM
 FROM DBC.COLUMNS
 WHERE DATABASENAME='##DBNAME##' ) TBL;
-
--- 3. ziskat zdrojove kody - zavolat na vsechno SHOW prikaz (vraci jeden sloupec, jeden radek)
---split
-
-SHOW TABLE '##DBNAME##'.'##TABLENAME##';
-
---split
-
-SHOW VIEW '##DBNAME##'.'##VIEWNAME##';
-
--- sql/teradata/queries/cmd.sql
+++ sql/teradata/queries/cmd.sql
@@ -1,8 +1,14 @@
--- procedury
-
-SELECT '##DBNAME##', creatorname, tablename from dbc.tables WHERE TABLEKIND = 'P'
-
+SELECT '##DBNAME##', creatorname, tablename from dbc.tables WHERE TABLEKIND = 'P';
 
 --split
 
-SHOW PROCEDURE ##DBNAME##.##PROCEDURENAME##
+SHOW PROCEDURE ##DBNAME##.##PROCEDURENAME##;
+
+--split
+
+SHOW TABLE ##DBNAME##.##TABLENAME##;
+
+--split
+
+SHOW VIEW ##DBNAME##.##VIEWNAME##;
+
