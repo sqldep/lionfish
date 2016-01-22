84e8017e60307ac28fde23bca660d425e7ed0af9
Jiri Sedlacek <sej@sqldep.com>
2016-01-22 14:17:52 +0100

    Much faster SELECT to export metadata about tables from Oracle

--- sql/oracle/tables/cmd.sql
+++ sql/oracle/tables/cmd.sql
@@ -2,61 +2,49 @@ select
   '##DBNAME##' as databaseName,
   x.owner as schemaName,
   x.table_name as tableName,
-  nvl2(w.object_name,'Y','N') as isView, -- type materialized view is a view too
+  decode(obj.object_type, 'VIEW','Y','MATERIALIZED_VIEW','Y','N') as isView,
   x.column_name as columnName,
   x.data_type||
-	case
-	  when x.data_type ='RAW' then '('|| x.data_length ||')'
-	  when x.data_precision is not null and nvl(x.data_scale,0)>0 then '('||x.data_precision||','||x.data_scale||')'
-	  when x.data_precision is not null and nvl(x.data_scale,0)=0 then '('||x.data_precision||')'
-	  when x.data_precision is null and x.data_scale is not null then '(38,'||x.data_scale||')'
-	  when x.char_length>0 then '('||x.char_length|| case x.char_used 
-													   when 'B' then ' byte'
-													   when 'C' then ' char'
-													   else null 
-													 end||')'
-	  end /*||decode(x.nullable, 'N', ' NOT NULL')*/ as dataType,
+    case
+      when x.data_type ='RAW' then '('|| x.data_length ||')'
+      when x.data_precision is not null and nvl(x.data_scale,0)>0 then '('||x.data_precision||','||x.data_scale||')'
+      when x.data_precision is not null and nvl(x.data_scale,0)=0 then '('||x.data_precision||')'
+      when x.data_precision is null and x.data_scale is not null then '(38,'||x.data_scale||')'
+      when x.char_length>0 then '('||x.char_length|| case x.char_used 
+                                                       when 'B' then ' byte'
+                                                       when 'C' then ' char'
+                                                       else null 
+                                                     end||')'
+      end /*||decode(x.nullable, 'N', ' NOT NULL')*/ as dataType,
   c.comments as comments
 from all_tab_cols x,
-	 all_col_comments c,
-	(select 
-	   owner,
-	   object_name,
-	   object_type
-	 from all_objects
-	 where object_type in ('MATERIALIZED VIEW', 'VIEW')) w, --  VIEWS
-	(select
-	   owner,
-	   object_name,
-	   object_type
-	 from all_objects 
-	 where  object_type in ('TABLE') ) t -- TABLES
-where c.column_name = x.column_name -- joining cols and colls somments
+     all_col_comments c,
+     all_objects obj
+where 
+  c.column_name = x.column_name -- joining cols and colls somments
   and c.owner =x.owner
   and c.table_name = x.table_name
-  and x.owner =t.owner(+) -- outer join tables
-  and x.table_name = t.object_name(+)
-  and x.owner =w.owner(+) -- outer join views 
-  and x.table_name = w.object_name(+) 
+  and x.owner =obj.owner
+  and x.table_name = obj.object_name 
+  and obj.object_type in ('TABLE','MATERIALIZED VIEW', 'VIEW')
   and x.owner NOT IN (
-	'APEX_040200',
-	'CTXSYS',
-	'DVF',
-	'DVSYS',
-	'GSMADMIN_INTERNAL',
-	'LBACSYS',
-	'MDSYS',
-	'OLAPSYS',
-	'ORCL',
-	'ORDDATA',
-	'ORDPLUGINS',
-	'ORDSYS',
-	'PUBLIC',
-	'SI_INFORMTN_SCHEMA',
-	'SYS',
-	'SYSTEM',
-	'WMSYS',
-	'XDB'
-	)
-
+    'APEX_040200',
+    'CTXSYS',
+    'DVF',
+    'DVSYS',
+    'GSMADMIN_INTERNAL',
+    'LBACSYS',
+    'MDSYS',
+    'OLAPSYS',
+    'ORCL',
+    'ORDDATA',
+    'ORDPLUGINS',
+    'ORDSYS',
+    'PUBLIC',
+    'SI_INFORMTN_SCHEMA',
+    'SYS',
+    'SYSTEM',
+    'WMSYS',
+    'XDB'
+    )
 order by x.owner, x.table_name, isView, x.column_id
