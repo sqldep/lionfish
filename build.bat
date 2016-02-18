7f50526fbc1c719a40c7e084fdfa65b4a5c19306
Jiri Sedlacek <sej@sqldep.com>
2016-02-18 11:02:20 +0000

    Add working version of build.bat - automated building process

--- build.bat
+++ build.bat
@@ -1,9 +1,17 @@
-rmdir SQLDep/bin/Release
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" /build SQLDepCmd/SQLDepCmd.csproj
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" /build SQLDep/SQLDep.csproj
-cp -r sql SQLDep/bin/Release
-cp export.bat SQLDep/bin/Release
-cp README.md SQLDep/bin/Release
-cp packages/Terradata/Teradata.net.dll SQLDep/bin/Release
-cp -r SQLDep/bin/Release SQLDep/bin/SQLdep
-zip sqldep.zip SQLDep/bin/SQLdep/
+set BUILDDIR=build_%TIME::=-%
+mkdir %BUILDDIR%
+
+rmdir /S/Q SQLDep\bin\Release
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" SQLDepCmd/SQLDepCmd.csproj  /build Release
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" SQLDep/SQLDep.csproj  /build Release
+
+xcopy SQLDepCmd\bin\Release\SQLDepCmd.exe SQLDep\bin\Release
+xcopy /s sql SQLDep\bin\Release\sql\
+xcopy export.bat SQLDep\bin\Release
+xcopy README.md SQLDep\bin\Release
+xcopy packages\Terradata\Teradata.Net.Security.Tdgss.dll SQLDep\bin\Release
+del /s SQLDep\bin\Release\*.pdb
+
+xcopy /s SQLDep\bin\Release %BUILDDIR%\SQLdep\
+cd %BUILDDIR%
+zip -r SQLdep.zip SQLdep\
