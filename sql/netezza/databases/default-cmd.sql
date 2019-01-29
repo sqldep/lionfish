f534b05113cd37d437d111a09a4593bc5b948f98
Tomas Kukan <kukant96@gmail.com>
2019-01-29 12:22:46 +0100

    sql codes

---
+++ sql/netezza/databases/default-cmd.sql
@@ -0,0 +1,4 @@
+/* IMPORTANT: If you modify this file, rename it to cmd.sql (so it won't be overwritten when you upgrade lionfish).
+Only cmd.sql will be used when both exist.*/
+
+SELECT database_name AS databasename FROM  INFORMATION_SCHEMA.databases;
